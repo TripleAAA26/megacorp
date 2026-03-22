72db4e3131e7b4a51f8a2161fad59f6c0eb71094
Azim <96.azim.96@gmail.com>
2026-03-22 21:26:36 +0500

    K: credits

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,9 @@
-# TODO: write the script
+echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
+grep -rE --color=always 'wfwrfwrfrfwrf'
+echo "\n====== CREDIT CARD SCAN COMPLETED ======"
+echo "\n====== SCANNING FOR SOCIAL SECURITY NUMBERS ======"
+grep -rE --color=always 'wfwrfwrfrfwrf'
+echo "\n====== SOCIAL SECURITY SCAN COMPLETED ======"
+echo "\n====== SCANNING FOR PHONE NUMBERS ======"
+grep -rE --color=always 'wfwrfwrfrfwrf'
+echo "\n====== PHONE SCAN COMPLETED ======"
